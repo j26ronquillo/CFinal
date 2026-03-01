381f6af9d5a2bef12cc8e50ec00b2213d1b91231
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-03-01 13:22:48 -0800

    Finished step one (menu) and implented a try-catch for step 9, fixes user input values that arent numeric.

---
+++ C#Final/C#Final/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace C_Final
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("***Welcome to the Game Character Creation System!***");
+			bool programMenu = true;
+			while (programMenu)
+			{
+				Console.WriteLine("1. Create New Character\n2. View all Characters\n3. Search Characters\n4. Display All Characters\n5. Exit Program");
+				Console.Write("Enter Choice: ");
+				try
+				{
+					int choice = int.Parse(Console.ReadLine());
+					switch (choice)
+					{
+						case 1:
+						case 2:
+						case 3:
+						case 4:
+						case 5: 
+							programMenu = false;
+							break;
+						default:
+							Console.WriteLine("ERROR: Invalid menu number.");
+							break;
+					}
+				}
+				catch (FormatException)
+				{ Console.WriteLine("ERROR: Please enter a valid numerical value."); }
+			}
+		}
+	}
+}
